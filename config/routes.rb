61303fa857d57b3f9c4375cd605ff03b5f1ef4e4
Kenta Watanabe <soukaijapan@gmail.com>
2017-02-23 13:54:11 +0000

    TO_Dive14

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users, controllers:{
+    registrations:"users/registrations",
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
   resources :pictures, only: [:index, :new, :create, :edit, :update, :destroy]
   root 'top#index'
   if Rails.env.development?
